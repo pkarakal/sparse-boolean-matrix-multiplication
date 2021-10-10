8fcfd537cc392129ce0b86697ea4b06ef4a4f10d
Pavlos Karakalidis <pavloc.kara@outlook.com>
2021-10-10 23:46:35 +0300

    feat(lib): add CSCMatrix struct

--- lib/bmm_lib.h
+++ lib/bmm_lib.h
@@ -16,6 +16,13 @@ namespace bmm_lib {
   void read_matrix(FILE* f, std::vector<uint32_t>& I, std::vector<uint32_t>& J,
 				   std::vector<uint32_t>& val, int& row_count, int& col_count);
 #endif
+  struct CSCMatrix {
+	std::vector<uint32_t> cscRow;
+	std::vector<uint32_t> cscColumn;
+	std::vector<uint32_t> c_values;
+	int row_count;
+	int col_count;
+  };
 } // namespace bmm_lib
 
 #endif // SPARSE_BOOLEAN_MATRIX_MULTIPLICATION_BMM_LIB_H
